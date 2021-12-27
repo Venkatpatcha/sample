951ab3a81088063c311c8421b89b7033c018beec
venkatpatcha <venkatpatcha@gmail.com>
2021-12-26 22:16:41 -0600

    New project addede

---
+++ src/sample1/samp4.java
@@ -0,0 +1,26 @@
+
+package sample1;
+import java.util.*;
+
+public class samp4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> al =new ArrayList<String>();
+		
+		al.add("Mango");
+		al.add("Banan");
+		al.add("Orange");
+		
+		for (String Family:al) {
+			
+			System.out.println(Family);
+		}
+		
+		for(String furit:al){
+		System.out.println(furit);
+		}	
+	
+		
+	}
+
---
+++ src/sample1/sampl56.java
@@ -0,0 +1,40 @@
+package sample1;
+
+public class sampl56 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[] a = {2, 4, 5, 1, 8, 3, 6, 3, 5,7} ;
+			int temp=0;
+			int j=0;
+			
+			for(int i=0;i<=a.length-i;i++)
+			{
+			
+				System.out.println(":i:"+i);
+				System.out.println("j" + j);
+			
+				while(j<a[i]) 
+				{
+					System.out.print(" " + a[j+1]);
+					j=j+1;
+					System.out.println(":j:"+j);
+				}
+				System.out.println();
+//				i=j;
+				
+//				System.out.println("i" + i);
+//				for(int j=i;j<a[i];j++) 
+//				{
+//					System.out.println("Vales" + a[j+1]);
+//					temp=j;
+//					System.out.println("temp"+temp);
+//				}
+//			i=temp+1;
+			
+			  }
+			}
+			
+	}
+
+
